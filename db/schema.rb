064cef9bfb58a1c6f71dcb4cfceacbf2b959a387
Lisa Nguyen <lisathuanguyen@gmail.com>
2018-05-05 17:21:49 -0700

    switching database from mysql to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_023543) do
+ActiveRecord::Schema.define(version: 2018_05_05_061716) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.integer "phone_number"
     t.integer "zipcode"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "weddings", force: :cascade do |t|
+    t.datetime "date"
+    t.string "location"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_weddings_on_user_id"
+  end
+
+  add_foreign_key "weddings", "users"
 end
