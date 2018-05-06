eefbd563519625e424dd1e83234203aaf619c2f3
Lisa Nguyen <lisathuanguyen@gmail.com>
2018-05-05 17:15:04 -0700

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, notice: 'Logged in!'
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_weddings_path(@user), notice: 'Logged in!'
     else
       flash.now.alert = 'Email or password is invalid.'
       render :new
